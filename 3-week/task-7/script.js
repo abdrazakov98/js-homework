e90c0f57c28f9a78ec6c5897194452a7938fc8ca
Nurlan Abdrazakov <nurlan.abdrazakov.kk@gmail.com>
2022-02-12 21:51:12 +0600

    add task7

---
+++ 3-week/task-7/script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#add-todo-form');
+const input = document.querySelector('#input-todo');
+const btnSubmit = document.querySelector('button');
+const todoList = document.querySelector('.todo-list');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+})
+
+btnSubmit.addEventListener('click', function() {
+    let myTodoList = document.createElement('div')
+    myTodoList.classList.add('todo-item')
+    todoList.append(myTodoList)
+    myTodoList.innerText = input.value
+    input.value = ''
+    
+    myTodoList.addEventListener('click', () => {
+        myTodoList.remove()
+    })
+})
